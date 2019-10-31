430c2dc1f5f15ff6ec2a7dcc3a574d4f48d196a1
Yu Heng Su <yuheng.su@mail.utoronto.ca>
2019-10-31 12:58:39 -0400

    Finished gallery, I think.......

--- js/gallery.js
+++ js/gallery.js
@@ -3,10 +3,15 @@ for (let i = 0; i <= max; i++) {
     //To create a circular column thingie
     var column = (".column-" + ((i % 4) + 1));
     let grid = document.querySelector(column);
-    let img = document.createElement('img');
+    let img = document.createElement("img");
+    let a = document.createElement("a");
 
     img.setAttribute("src", "images/club/picture" + i + ".png");
     img.setAttribute("class", "images");
 
-    grid.appendChild(img);
+    a.setAttribute("data-lightbox", "badminton")
+    a.setAttribute("href", "images/club/picture" + i + ".png");
+
+    a.append(img);
+    grid.appendChild(a);
 }
