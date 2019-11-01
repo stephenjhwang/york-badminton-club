2dc570b8c691fe57588203aec1b558d24f6de403
StephenH <StephenHwang156@outlook.com>
2019-11-01 00:04:39 -0400

    Done home page except gist

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+    $('#faq-toggle').click(function() {
+        if ( $(this).hasClass('expand') ) {
+            $(this).removeClass('expand').addClass('collapse');
+            //$('#faq').css('max-height', 'none');
+            let faq = document.getElementById("faq");
+            faq.style.maxHeight = faq.scrollHeight + "px";
+            
+        }
+        else if ( $(this).hasClass('collapse') ) {
+            $(this).removeClass('collapse').addClass('expand');
+            //$('#faq').css('max-height', '175px');
+            let faq = document.getElementById("faq");
+            faq.style.maxHeight = null;
+        }
+    });
+});
